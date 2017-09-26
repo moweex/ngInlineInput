60cf7673ff86ee2a88034b8162db5550a33c7821
Mounir <mohamed.mounir@mowesolutions.com>
2017-09-26 18:23:55 +0200

    Added select field, and email field with validators

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { InlineInputComponent } from './inline-input/inline-input.component';
 
 
 import { AppComponent } from './app.component';
 import { ProfileCardComponent } from './profile-card/profile-card.component';
-import { MatCardModule, MatButtonModule, MatToolbarModule, MatIconModule, MatInputModule, MatDatepickerModule } from '@angular/material';
+import { MatCardModule,
+         MatButtonModule,
+         MatToolbarModule,
+         MatIconModule,
+         MatInputModule,
+         MatDatepickerModule,
+         MatSelectModule,
+         MatAutocompleteModule
+       } from '@angular/material';
 
 
 
@@ -26,7 +34,10 @@ import { MatCardModule, MatButtonModule, MatToolbarModule, MatIconModule, MatInp
     MatToolbarModule,
     MatIconModule,
     MatInputModule,
-    FormsModule
+    FormsModule,
+    ReactiveFormsModule,
+    MatSelectModule,
+    MatAutocompleteModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/inline-input/inline-input.component.ts
+++ src/app/inline-input/inline-input.component.ts
@@ -1,20 +1,68 @@
 import { Component, OnInit, Input } from '@angular/core';
+import { FormControl, Validators } from '@angular/forms';
+import {Observable} from 'rxjs/Observable';
+import 'rxjs/add/operator/startWith';
+import 'rxjs/add/operator/map';
+
+const EMAIL_REGEX = /^[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+
 
 @Component({
   selector: 'inline-input',
   templateUrl: './inline-input.component.html',
   styleUrls: ['./inline-input.component.css']
 })
+
 export class InlineInputComponent implements OnInit {
 
 @Input() type:string;
 @Input() placeholder:string;
 @Input() value:string;
 @Input() editMode:boolean;
+@Input() isDisabled:boolean;
+@Input() isRequired:boolean;
 
-  constructor() { }
+  emailFormControl = new FormControl('', [
+    Validators.pattern(EMAIL_REGEX)
+  ]);
+
+  selectOptions = [
+    {value: 'jazz', viewValue: 'Jazz'},
+    {value: 'reggae', viewValue: 'Reggae'},
+    {value: 'country', viewValue: 'Country'}
+  ];
+
+  autocompleteOptions = [
+    {value: 'arkansas', viewValue: 'Arkansas'},
+    {value: 'california', viewValue: 'California'},
+    {value: 'florida', viewValue: 'Florida'},
+    {value: 'Texas', viewValue: 'Texas'}
+  ];
+
+
+
+stateCtrl: FormControl;
+filteredStates: Observable<any[]>;
+
+
+  constructor() {
+    this.stateCtrl = new FormControl();
+    this.filteredStates = this.stateCtrl.valueChanges
+        .startWith(null)
+        .map(state => state ? this.filterStates(state) : this.autocompleteOptions.slice());
+  }
+
+  filterStates(name: string) {
+    return this.autocompleteOptions.filter(state =>
+      state.viewValue.toLowerCase().indexOf(name.toLowerCase()) === 0);
+  }
+
+
 
   ngOnInit() {
+
   }
+
+
 
 }
